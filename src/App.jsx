84d88ad2e320d805e778ed4cdd5b0b0531c15733
Ali Ghasemi <ali.gha3mi75@mail.com>
2024-09-09 23:35:39 +0330

    base structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-center mt-3 font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
